d8f69fdd9f46fa339c6c7a96a99eae2bc7489e1b
Shun <ogc.ss424@gmail.com>
2021-01-02 23:52:19 +0900

    finish 05

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,58 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@1000
+D=A
+@num_pix
+M=D
+(LOOP)
+	@KBD
+	D=M
+	@LOOP
+	D;JEQ
+	@i_fill_pix
+	M=0
+	@i_clear_pix
+	M=0
+	(SCREEN_FILL_LOOP)
+		@i_fill_pix
+		D=M
+		@num_pix
+		D=M-D
+		@SCREEN_FILL_END
+		D;JEQ
+		
+		@i_fill_pix
+		D=M
+		@SCREEN
+		A=D+A
+		M=1
+		@i_fill_pix
+		M=M+1
+		@SCREEN_FILL_LOOP
+		0;JMP
+	(SCREEN_FILL_END)
+		@i_fill_pix
+		M=0
+	(SCREEN_CLEAR_LOOP)
+		@i_clear_pix
+		D=M
+		@num_pix
+		D=M-D
+		@SCREEN_CLEAR_END
+		D;JEQ
+		
+		@i_clear_pix
+		D=M
+		@SCREEN
+		A=D+A
+		M=0
+		@i_clear_pix
+		M=M+1
+		@SCREEN_CLEAR_LOOP
+		0;JMP
+	(SCREEN_CLEAR_END)
+		@i_clear_pix
+		M=0
+		@LOOP
+		0;JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,43 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+//テスト用
+//@3
+//D=A
+//@R0
+//M=D
+//@4
+//D=A
+//@R1
+//M=D
+
+//初期設定
+@i
+M=1
+@R2
+M=0
+(LOOP)
+	//終了判定
+	@R1
+	D=M
+	@i
+	D=D-M
+	@END
+	D;JLT
+	
+	//乗算処理
+	@R2
+	D=M
+	@R0
+	D=D+M
+	@R2
+	M=D
+	@i
+	M=M+1
+	@LOOP
+	0;JMP
+	
+(END)
+	@END
+	0;JMP
